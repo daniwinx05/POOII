2fa4595242a46505242375c9c63095ce47e4dee5
daniwinx05 <danielleg.ferreira59@gmail.com>
2025-08-12 08:25:01 -0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta cc01 = new Conta(100, 1, 100);
+            Cliente cl01 = new Cliente("Dani", "123", "Cariacica");
+
+            cc01.Corrente = cl01;
+
+            Console.WriteLine($"Saldo: {cc01.saldo}");
+            cc01.Depositar(200);
+            Console.WriteLine($"Saldo: {cc01.saldo}");
+
+        }
+    }
+}
