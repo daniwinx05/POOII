1706724fe327226e836826d3c1f324692fa10af7
daniwinx05 <danielleg.ferreira59@gmail.com>
2025-08-12 08:41:23 -0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Cliente
+    {
+        public string nome { get; set; }
+        public string cpf { get; set; }
+        public string endereco { get; set; }
+
+        public Cliente(string nome, string cpf, string endereco) 
+        {
+            nome = nome ?? throw new ArgumentNullException(nameof(nome));
+            cpf = cpf ?? throw new ArgumentNullException( nameof(cpf));
+            endereco = endereco ?? throw new ArgumentNullException(nameof(endereco));
+        }
+
+        
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Conta.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Conta
+    {
+        public double limite { get; set; }
+        public int numero { get; set; }
+        public double saldo { get; set; }
+
+        public Cliente Corrente {get; set;}
+
+        public Conta(double limite, int numero, double saldo) {
+            this.limite = limite;
+            this.numero = numero;
+            this.saldo = saldo;
+        
+        }
+
+
+        public void Depositar(double valor) => saldo += valor;
+
+        public bool Sacar(double valor) 
+        {
+            if (saldo > valor) 
+            {
+                saldo -= valor; 
+                return true;
+            } 
+            return false;
+        }
+
+        private bool VerificarTransacao(double valor)
+        { 
+        
+            return saldo > valor;
+        }
+
+
+    }
+}
