9102ea9e61c5849b1b2a328997f7b6304e981681
daniwinx05 <danielleg.ferreira59@gmail.com>
2025-08-29 08:53:31 -0300

    Add files via upload

---
+++ Atividade2908/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Atividade2908
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Clique_Aqui_Click(object sender, RoutedEventArgs e)
+        {
+            var objetoNovo = new Window
+            {
+                Title = "oi",
+                Width = 200,
+                Height = 200,
+            };
+
+            var layout = new StackPanel();
+
+            var t = new TextBlock { 
+                
+                
+              Text = "Bom dia !",
+
+            };
+            layout.Children.Add(t);
+            objetoNovo.Content = layout;
+            objetoNovo.Show();
+        }
+        
+           
+
+
+
+       
+    }
+}
